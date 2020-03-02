322d2d4ebe767f4981427c01eea75dab3ea0c1a8
Vorobeyko <sashavorobey95@gmail.com>
2020-03-02 23:03:21 +0300

    add just migrate script and test

---
+++ migrate.js
@@ -0,0 +1,32 @@
+const j = require('jscodeshift');
+
+module.exports = (fileInfo) => {
+  const newSource = j(fileInfo.source).find(j.ExpressionStatement, {
+    expression: {
+      callee: {
+        name: 'Scenario',
+      },
+    },
+  })
+    .find(j.ArrowFunctionExpression)
+    // eslint-disable-next-line array-callback-return
+    .filter(p => {
+      const root = p.value;
+      for (const param of root.params) {
+        if (param.type === 'ObjectPattern') {
+          // console.log(param);
+        }
+        return param.type !== 'ObjectPattern';
+      }
+    })
+    .replaceWith(p => {
+      const root = p.value;
+      const firstParam = root.params[0];
+      const lastParam = root.params[root.params.length - 1];
+
+      firstParam.name = `{ ${firstParam.name}`;
+      lastParam.name = `${lastParam.name} }`;
+      return root;
+    });
+  return newSource.toSource();
+};
